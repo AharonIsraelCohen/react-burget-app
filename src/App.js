765ef416c2368affbfa2e7241ce7ffa4c7cf83a2
Aharon <israel245@gmail.com>
2021-02-09 21:24:48 +0200

    The begining of  the burger builder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout'
+import Layout from './components/Layout/Layout';
+import BurderBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          Test
+          <BurderBuilder></BurderBuilder>
         </Layout>
 
       </div>
